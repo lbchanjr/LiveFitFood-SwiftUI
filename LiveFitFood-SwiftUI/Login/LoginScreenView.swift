1471ab9c6e7b5de2ed02fa0f519b70a527212cc4
Louise Chan <lbchan525@gmail.com>
2021-04-09 01:53:41 -0400

    Login screen refactoring

--- LiveFitFood-SwiftUI/Login/LoginScreenView.swift
+++ LiveFitFood-SwiftUI/Login/LoginScreenView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 import CoreData
 
 struct LoginScreenView: View {
-    //@State var userLoggedIn = false
     @State var registerUser = false
     @EnvironmentObject var loginViewModel: LoginViewModel
     
@@ -39,50 +38,14 @@ struct LoginScreenView: View {
             PasswordPromptView(registerUser: $registerUser, textLabel: .constant("Password"), passwordPlaceholder: .constant("Enter password"), passwordInput: $loginViewModel.password)
             
             if !registerUser {
-                HStack {
-                    Spacer()
-                    Button(action: {
-                        signInButtonPressed()
-                    }) {
-                        Text("Sign-in / Sign-up")
-                            .font(.title2)
-                            .padding(.all)
-                            .background(Color.blue)
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
-                    }
-                    .opacity(loginViewModel.validEmail ? 1.0: 0.5)
-                    .fullScreenCover(isPresented: $userStatus.isLoggedIn) {
-//                        Button("Logout") {
-//                            userLoggedIn.toggle()
-//                        }
-//                        .padding(10)
-//                        .background(Color.blue)
-//                        .foregroundColor(.white)
-//                        .cornerRadius(10)
-                        loginViewModel.toWelcomeScreenView
-                    }
-                    .disabled(!loginViewModel.validEmail)
-                    
-                    Spacer()
-                }
-                .padding(.top, 30)
-                Toggle("Save username and password?", isOn: $loginViewModel.saveLoginInfo)
-                    .padding()
+                LoginButtonViews(registerUser: $registerUser)
             } else {
                 RegistrationScreenView(registerUser: $registerUser)
             }
             
             Spacer()
             
-            HStack {
-                Spacer()
-                Text(loginViewModel.message)
-                    .foregroundColor(.red)
-                    .padding(.bottom)
-                    .font(.headline)
-                Spacer()
-            }
+            LoginScreenMessage()
             
         }
         .padding(.horizontal)
@@ -91,22 +54,6 @@ struct LoginScreenView: View {
             self.hideKeyboard()
         }
     }
-    
-    
-    private func signInButtonPressed() {
-        switch(loginViewModel.processLogin()) {
-        case .loginOK:
-            userStatus.isLoggedIn.toggle()
-        case .newUser:
-            registerUser = true
-        
-        default:
-            print("Login result: \(loginViewModel.processLogin())")
-        }
-        
-        
-    }
-    
 }
 
 struct LoginScreenView_Previews: PreviewProvider {
@@ -125,4 +72,110 @@ struct LoginScreenView_Previews: PreviewProvider {
     }
 }
 
+struct HeaderView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Image("livefitfood")
+                    .resizable()
+                    .scaledToFit()
+                        .frame(minHeight: UIScreen.main.bounds.height * 0.1, maxHeight: UIScreen.main.bounds.height * 0.125)
+                    .padding(.vertical)
+                Spacer()
+            }
+            
+            HStack {
+                Spacer()
+                Text("Live Fit Mealkit Ordering App")
+                    .font(.title3)
+                Spacer()
+            }
+            .padding(.top, -15)
+        }
+    }
+}
 
+struct PasswordPromptView: View {
+    @Binding var registerUser: Bool
+    @Binding var textLabel: String
+    @Binding var passwordPlaceholder: String
+    @Binding var passwordInput: String
+    
+    var body: some View {
+        VStack (alignment: .leading){
+            Text(textLabel)
+                .font(.subheadline)
+                .padding(.top, registerUser ? 5 : 10)
+                SecureField(passwordPlaceholder, text: $passwordInput)
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 5)
+                    .minimumScaleFactor(0.8)
+                    .background(Color(.secondarySystemBackground))
+        }
+    }
+}
+
+struct LoginScreenMessage: View {
+    @EnvironmentObject var loginViewModel: LoginViewModel
+    
+    var body: some View {
+        HStack {
+            Spacer()
+            Text(loginViewModel.message)
+                .foregroundColor(.red)
+                .padding(.bottom)
+                .font(.headline)
+            Spacer()
+        }
+    }
+}
+
+struct LoginButtonViews: View {
+    @EnvironmentObject var loginViewModel: LoginViewModel
+    @EnvironmentObject var userStatus: UserStatus
+    
+    @Binding var registerUser: Bool
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Button(action: {
+                    signInButtonPressed()
+                }) {
+                    Text("Sign-in / Sign-up")
+                        .font(.title2)
+                        .padding(.all)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                .opacity(loginViewModel.validEmail ? 1.0: 0.5)
+                .fullScreenCover(isPresented: $userStatus.isLoggedIn) {
+                    loginViewModel.toWelcomeScreenView
+                }
+                .disabled(!loginViewModel.validEmail)
+                
+                Spacer()
+            }
+            .padding(.top, 30)
+            Toggle("Save username and password?", isOn: $loginViewModel.saveLoginInfo)
+                .padding()
+        }
+    }
+    
+    private func signInButtonPressed() {
+        switch(loginViewModel.processLogin()) {
+        case .loginOK:
+            userStatus.isLoggedIn.toggle()
+        case .newUser:
+            registerUser = true
+        
+        default:
+            print("Login result: \(loginViewModel.processLogin())")
+        }
+        
+        
+    }
+}
